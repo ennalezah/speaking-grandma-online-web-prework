845ac23f6c59ceb3c1b89b16fc17d2ad745e516a
Hazel Anne Villareal <hae.villareal@gmail.com>
2019-04-04 00:58:00 +0000

    Automatically backed up by Learn

--- grandma.rb
+++ grandma.rb
@@ -15,8 +15,8 @@
 def speak_to_grandma(phrase)
   if phrase == phrase.upcase
     puts "NO, NOT SINCE 1938!"
-    elsif phrase != phrase.upcase
-      puts ""
+    elsif phrase == 
+      
   end
   
 end
