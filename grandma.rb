0cab75e811b74c23468c8815be6342c31eb5a6b1
Hazel Anne Villareal <hae.villareal@gmail.com>
2019-04-04 00:56:17 +0000

    Automatically backed up by Learn

--- grandma.rb
+++ grandma.rb
@@ -14,7 +14,9 @@
 
 def speak_to_grandma(phrase)
   if phrase == phrase.upcase
-    puts "HUH?! SPEAK UP, SONNY!"
+    puts "NO, NOT SINCE 1938!"
+    elsif phrase != phrase.upcase
+      puts ""
   end
   
 end
